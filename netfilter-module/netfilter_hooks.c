d63d2c94082daad99f435a07440fd61063151d31
reemho <reemho@edu.jmc.ac.il>
2025-08-27 22:07:58 +0000

    completed stage1 : filtering packets by ip addresses

---
+++ netfilter-module/netfilter_hooks.c
@@ -0,0 +1,63 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/netfilter.h>   
+#include <linux/ip.h>                
+#include <linux/netfilter_ipv4.h>                
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Re'em Hoisman");
+MODULE_DESCRIPTION("Netfilter Kernel Module for blocking packets by IP address");
+
+static unsigned int blocked_src_ips[] = {0x7F000001};
+static int blocked_src_ips_size = 1;
+
+static unsigned int blocked_dest_ips[] = {0x08080808};
+static int blocked_dest_ips_size = 1;
+
+static unsigned int packet_hook(void *priv, struct sk_buff *skb,
+                                const struct nf_hook_state *state){
+    if (!skb) return NF_ACCEPT;
+   
+    const struct iphdr *ip_header;
+
+    ip_header = ip_hdr(skb);
+
+    if (!ip_header) return NF_ACCEPT;
+
+    for (int i = 0; i < blocked_src_ips_size; i++) {
+        if (ip_header->saddr == htonl(blocked_src_ips[i])) {
+            printk(KERN_INFO "The source IP %pI4 is blocked\n", &ip_header->saddr);
+            return NF_DROP;
+        }
+    }
+
+    for (int i = 0; i < blocked_dest_ips_size; i++) {
+        if (ip_header->daddr == htonl(blocked_dest_ips[i])) {
+            printk(KERN_INFO "The dest IP %pI4 is blocked\n", &ip_header->daddr);
+            return NF_DROP;
+        }
+    }
+
+    return NF_ACCEPT;
+}
+
+static struct nf_hook_ops nfho = {
+    .hook = packet_hook,
+    .pf = PF_INET,
+    .hooknum  = NF_INET_PRE_ROUTING, 
+    .priority = NF_IP_PRI_FIRST, 
+};
+
+static int __init my_netfilter_init(void){
+    nf_register_net_hook(&init_net, &nfho);
+    printk(KERN_INFO "Netfilter Module: Loaded and hook registered.\n");
+    return 0;
+}
+
+static void __exit my_netfilter_exit(void){
+    nf_unregister_net_hook(&init_net, &nfho);
+    printk(KERN_INFO "Netfilter Module: Unloaded and hook unregistered.\n");
+}
+
+module_init(my_netfilter_init);
+module_exit(my_netfilter_exit);
